5b0ff40b5a1114978cc6978aef346c36aeb9230c
TracyNK <95154345+TracyKien@users.noreply.github.com>
2022-02-25 21:08:06 -0600

    Add files via upload

--- Pewlett-Hackard-Analysis/Queries/schema.sql
+++ Pewlett-Hackard-Analysis/Queries/schema.sql
@@ -258,3 +258,38 @@ INNER JOIN departments as d
 	ON (de.dept_no = d.dept_no)
 WHERE d.dept_name IN ('Sales', 'Development')
 ORDER BY ce.emp_no;
+
+SELECT ce.emp_no,
+	   ce.first_name,
+       ce.last_name,
+	   t.title,
+	   t.from_date,
+	   t.to_date
+INTO ret_titles
+FROM current_emp as ce
+	INNER JOIN titles as t
+		ON (ce.emp_no = ti.emp_no)
+ORDER BY ce.emp_no;
+
+DROP TABLE ret_titles CASCADE;
+
+SELECT 	e.emp_no
+		,e.first_name
+		,e.last_name
+		,t.title
+		,t.from_date
+		,t.to_date
+INTO retirement_titles		
+FROM employees AS e INNER JOIN 
+titles AS t ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+--INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no ASC, to_date DESC;
+
