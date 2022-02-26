99031693e9d8e5346a041b325422632cd811ad50
TracyNK <95154345+TracyKien@users.noreply.github.com>
2022-02-25 21:20:34 -0600

    Add files via upload

--- Pewlett-Hackard-Analysis/Queries/Employee_Database_Challenge.sql
+++ Pewlett-Hackard-Analysis/Queries/Employee_Database_Challenge.sql
@@ -273,17 +273,19 @@ ORDER BY ce.emp_no;
 
 DROP TABLE ret_titles CASCADE;
 
-SELECT 	e.emp_no
-		,e.first_name
-		,e.last_name
-		,t.title
-		,t.from_date
-		,t.to_date
+SELECT 	e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title,
+		t.from_date,
+		t.to_date
 INTO retirement_titles		
 FROM employees AS e INNER JOIN 
 titles AS t ON e.emp_no = t.emp_no
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
+
+
 
 SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
@@ -299,3 +301,17 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY TITLE
 Order BY COUNT(title) DESC;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+d.from_date,
+d.to_date,
+t.title
+INTO mentorship_eligibility
+FROM employees as e INNER JOIN
+dept_emp as d on e.emp_no = d.emp_no INNER JOIN
+titles as t on e.emp_no = t.emp_no
+WHERE d.to_date = '9999-01-01' and e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
